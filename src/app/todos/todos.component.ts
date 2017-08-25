5695a911f552d22d1facd44650cb84066ac11df9
Leandro Ramos <leoramos680@gmail.com>
2017-08-25 14:16:07 -0300

    Editar Tarefas

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -11,7 +11,9 @@ export class TodosComponent implements OnInit {
 
   constructor(private todosService: TodosService) { }
 
-  todos:Todo[];
+  todos: Todo[]
+
+  openEdit: object = []
 
   // teste(id){
   //   let text = "change!!";
@@ -20,23 +22,35 @@ export class TodosComponent implements OnInit {
 
   //   this.todosService.editTodo(id, text)
   // }
-
-  remove(id){
-    this.todosService.removeTodo(id);
+  teste() {
+    alert('double-click')
+  }
+  remove(id) {
+    this.todosService.removeTodo(id)
   }
 
- mouse(){
-   alert("mouse passou em cima daqui!!")
- }
- 
-  completed(id, todo){
-    todo.completed = !todo.completed;
-    this.todosService.editTodo(id, todo);
+  mouse() {
+    alert("mouse passou em cima daqui!!")
+  }
+
+  edit(id, newTodo) {
+    let todo = this.todos[id]
+    todo.text = newTodo
     
+    this.openEdit[id] = false
+    this.todosService.editTodo(id, todo)
+
+
+  }
+
+  completed(id, todo) {
+    todo.completed = !todo.completed
+    this.todosService.editTodo(id, todo)
+
   }
 
   ngOnInit() {
-    this.todos = this.todosService.getTodos();
+    this.todos = this.todosService.getTodos()
   }
 
 }
